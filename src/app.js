d1fb2c128950951b1610fb2eed668d00c0f88e16
Sasongko Adi <sasongko.adinugroho@qmail.id>
2020-10-28 00:04:50 +0800

    Setup for deploy to heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const app = express()
-const port = 3500 
+const port = process.env.PORT || 3500
 //Define request method
 const geocode = require('./Utility/geocode')
 const forecast = require('./Utility/weather')
